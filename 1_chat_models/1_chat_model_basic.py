b188224952a976f4668e29d602fb6a05742483c4
Syed Shahzer <syedshahzer@Syeds-MacBook-Air.local>
2025-05-17 01:24:07 +0530

    modified code to use openrouter api key

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -1,14 +1,21 @@
-# Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/
-# OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
-
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-
+from os import getenv
 # Load environment variables from .env
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# model = ChatOpenAI(model="gpt-4o")
+
+# model = ChatOpenRouter(model="meta-llama/llama-3.3-8b-instruct:free")
+model = ChatOpenAI(
+    # api_key=getenv("OPENROUTER_API_KEY"),
+  openai_api_key=getenv("OPENROUTER_API_KEY"),
+  openai_api_base="https://openrouter.ai/api/v1",
+#   model_name="meta-llama/llama-3.3-8b-instruct:free",
+model_name="nousresearch/deephermes-3-mistral-24b-preview:free"
+
+)
 
 # Invoke the model with a message
 result = model.invoke("What is 81 divided by 9?")
