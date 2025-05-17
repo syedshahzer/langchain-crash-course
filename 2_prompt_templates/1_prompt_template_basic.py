a530e9db321d62b3795d28a61e3b826731ae1432
Syed Shahzer <syedshahzer@Syeds-MacBook-Air.local>
2025-05-17 16:51:50 +0530

    change model to gemini

--- 2_prompt_templates/1_prompt_template_basic.py
+++ 2_prompt_templates/1_prompt_template_basic.py
@@ -22,7 +22,7 @@ from langchain_core.messages import HumanMessage
 # print(prompt)
 
 
-# PART 3: Prompt with System and Human Messages (Using Tuples)
+# # PART 3: Prompt with System and Human Messages (Using Tuples)
 # messages = [
 #     ("system", "You are a comedian who tells jokes about {topic}."),
 #     ("human", "Tell me {joke_count} jokes."),
@@ -32,24 +32,24 @@ from langchain_core.messages import HumanMessage
 # print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
 # print(prompt)
 
-# # Extra Informoation about Part 3.
-# # This does work:
-# messages = [
-#     ("system", "You are a comedian who tells jokes about {topic}."),
-#     HumanMessage(content="Tell me 3 jokes."),
-# ]
-# prompt_template = ChatPromptTemplate.from_messages(messages)
-# prompt = prompt_template.invoke({"topic": "lawyers"})
-# print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
-# print(prompt)
+# Extra Informoation about Part 3.
+# This does work:
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    HumanMessage(content="Tell me 3 jokes."),
+]
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic": "lawyers"})
+print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+print(prompt)
 
 
 # This does NOT work:
-messages = [
-    ("system", "You are a comedian who tells jokes about {topic}."),
-    HumanMessage(content="Tell me {joke_count} jokes."),
-]
-prompt_template = ChatPromptTemplate.from_messages(messages)
-prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
-print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
-print(prompt)
+# messages = [
+#     ("system", "You are a comedian who tells jokes about {topic}."),
+#     HumanMessage(content="Tell me {joke_count} jokes."),
+# ]
+# prompt_template = ChatPromptTemplate.from_messages(messages)
+# prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+# print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+# print(prompt)
--- 2_prompt_templates/2_prompt_template_with_chat_model.py
+++ 2_prompt_templates/2_prompt_template_with_chat_model.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables from .env
 load_dotenv()
 
-# Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+# Create a ChatGoogleGenerativeAI model
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 # PART 1: Create a ChatPromptTemplate using a template string
 print("-----Prompt from Template-----")
